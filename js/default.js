432de556935321601b5aed14789fcd7d39b0b480
kumataniwatanabe03 <38421097+kumataniwatanabe03@users.noreply.github.com>
2018-11-12 18:05:10 +0900

    Update default.js

--- js/default.js
+++ js/default.js
@@ -1,12 +1,3 @@
-// ダイアログを表示
-var result = window.confirm('私は一体何の数字を思い浮かべているのか、当ててみてください！');
-
-//　結果を表示
-if(result){
- document.getElementById('choice').textContent = 'OKが押されました！頑張ってみてね！';
-}else{
- document.getElementById('choice').textContent = 'キャンセルが押されました。またの挑戦をお待ちしております！'; }
-
 //答え
 var number = Math.floor(Math.random()*2);
 var answer = parseInt(window.prompt('0～2の数字を入力してね！'))
