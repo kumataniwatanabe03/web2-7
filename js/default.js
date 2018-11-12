6eb1ceeb30b76df152746f0f9391941f2293f9d9
kumataniwatanabe03 <38421097+kumataniwatanabe03@users.noreply.github.com>
2018-11-12 17:46:30 +0900

    Update default.js

--- js/default.js
+++ js/default.js
@@ -1,4 +1,7 @@
 // ダイアログを表示
 var result = window.confirm('私は一体何の数字を思い浮かべているのか、当ててみてください！');
 //　結果を表示
-document.getElementById('choice').textContent = result;
+if(result){
+ document.getElementById('choice').textContent = 'OKが押されました';
+}else{
+ document.getElementById('choice').textContent = 'キャンセルが押されました'; }
